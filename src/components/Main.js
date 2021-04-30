02359197385d1d88eb1baf0293e956e7a4b95d6b
Simona <simona.laban@gmail.com>
2021-04-30 11:49:18 +0200

    first commit

---
+++ src/components/Main.js
@@ -0,0 +1,10 @@
+import React from 'react'
+// import "../style/styles.css";
+
+function Main() {
+    return (
+        <p>Main</p>
+    )
+}
+
+export default Main
---
+++ src/components/Navigation.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import "./styles.css";
+import "./colors.css";
+import Button from 'react-bootstrap/Button'
+
+function Navigation() {
+    return (
+      <Navbar className='bg-drk' variant="dark" collapseOnSelect expand="lg" >
+        <Navbar.Brand href="#home">
+          <img
+            src="/logo.png"
+            width="45"
+            height="45"
+            className="d-inline-block align-top"
+          />
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+          <Nav className="d-flex mr-5">
+            <Button className="mr-4 nav-custom-button" variant="outline-warning">Create Campaign</Button>{' '}
+            <Nav.Link href="#Hub">The Hub</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Navbar>
+    )
+}
+
+export default Navigation
